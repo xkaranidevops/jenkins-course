44a6c2cb93cf623a8f718120e192c838e8afe0b9
xkaranidevops <77978073+xkaranidevops@users.noreply.github.com>
2021-08-04 22:28:11 +0530

    Update nodejsdocker.groovy
    Modify dockerhub

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('1073100xkarani/docker-node-js-app')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
